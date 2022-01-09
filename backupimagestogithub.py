a1c11aec56e5a267f34dfb34584375f8cb5f99c2
Rohith Kumar Jayana <jayanarohith7@gmail.com>
2022-01-09 19:40:26 +0530

    Add Readme

--- backupimagestogithub.py
+++ backupimagestogithub.py
@@ -4,3 +4,12 @@ mypath = "D:\softwares\wallpapers"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 print(onlyfiles)
+
+readmefile = open('README.md', 'a')
+
+for image in onlyfiles:
+    htmlcode = "<img src=\""+ image + "\"> <br>"
+    readmefile.write(htmlcode)
+    readmefile.write('\n')
+
+readmefile.close()
