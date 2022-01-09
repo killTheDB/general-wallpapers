25fa38582899fbf6739a5d09bf3f90fa2f090e14
Rohith Kumar Jayana <jayanarohith7@gmail.com>
2022-01-09 20:03:09 +0530

    Add Readme

--- backupimagestogithub.py
+++ backupimagestogithub.py
@@ -6,14 +6,14 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 print(onlyfiles)
 
 readmefile = open('README.md', 'a')
-readmefile.write("<div class=\"row\" style=\"display: flex\">")
+readmefile.write("<div class=\"row\" style=\"display: flex\"> \n")
 for image in onlyfiles:
     readmefile.write("<div class=\"column\" style=\"flex: 33.33%; padding: 5px\">")
     readmefile.write('\n')
-    htmlcode = "<img src=\""+ image + "\" width=\"400\" height=\"400\"> <br>"
+    htmlcode = "<img src=\""+ image + "\" width=\"100%\"> <br>"
     readmefile.write(htmlcode)
     readmefile.write('\n')
-    readmefile.write("</div>")
+    readmefile.write("</div> \n")
 
 readmefile.write("</div>")
 readmefile.close()
