02fda50c805bf1760f97be614759a2a8d7360159
Rohith Kumar Jayana <jayanarohith7@gmail.com>
2022-01-16 23:31:01 +0530

    Regular Update

--- backupimagestogithub.py
+++ backupimagestogithub.py
@@ -1,11 +1,15 @@
 from os import listdir, read
 from os.path import isfile, join
+import base64
+from github import Github
+from github import InputGitTreeElement
+
 mypath = "D:\softwares\wallpapers"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 print(onlyfiles)
 
-readmefile = open('README.md', 'a')
+readmefile = open('README.md', 'w')
 readmefile.write("<div class=\"row\" style=\"display: flex\"> \n")
 for image in onlyfiles:
     readmefile.write("<div class=\"column\" style=\"flex: 33.33%; padding: 5px\">")
@@ -17,3 +21,4 @@ for image in onlyfiles:
 
 readmefile.write("</div>")
 readmefile.close()
+
